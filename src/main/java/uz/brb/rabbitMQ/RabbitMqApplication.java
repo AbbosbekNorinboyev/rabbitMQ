0313c1d22fc356936383248ae234a61487a5b64f
AbbosbekNorinboyev <norinboyevabboskhan002@gmail.com>
2026-04-30 11:50:22 +0500

    fix rabbitmq

--- src/main/java/uz/brb/rabbitMQ/RabbitMqApplication.java
+++ src/main/java/uz/brb/rabbitMQ/RabbitMqApplication.java
@@ -1,9 +1,11 @@
 package uz.brb.rabbitMQ;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class RabbitMqApplication {
 
     public static void main(String[] args) {
